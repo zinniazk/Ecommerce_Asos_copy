bef32864575df11a3ce95ce2a6baf662cf53ca58
zinniazk <zinniak_91@hotmail.com>
2025-03-01 09:30:17 +0000

    Added RemoveItem in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,15 @@
 import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux";
+import { removeItem } from "../utils/cartSlice";
 
 const Cart = () =>{
 
     const cartItems = useSelector((store)=>store.cart.items);
     console.log(cartItems);
+    const dispatch=useDispatch();
+    function handleRemoveItem(item){
+        dispatch(removeItem(item));
+    }
 
     return(
         <div>
@@ -12,6 +18,7 @@ const Cart = () =>{
                     <>
                     <li>{item.name}</li>
                     <img src={item.imgsrc} className="card-img"></img>
+                    <button onClick={()=>handleRemoveItem(item)} className="btn-cart">Remove item</button>
                     </>
                     
                 ))
