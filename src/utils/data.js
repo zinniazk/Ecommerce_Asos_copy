39de90d70fb72e513e4aeb6fae6681b62f654aa6
zinniazk <zinniak_91@hotmail.com>
2025-05-04 12:46:39 +0100

    Updated pathname of images to add forward slash

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,11 +1,11 @@
 export const asos_clothes_men_all=[
-    {id: 101, name: "Aape By A Bathing Ape Now knitted jumper in black", imgsrc: "Images/207571408-2.avif", price:170, category: "Jackets"},
-    {id: 102, name: "Jack & Jones oversize quarter zip sweatshirt in grey", imgsrc: "Images/men_img_2_jnj.avif", price:32, category: "Tops"},
-    {id: 103, name: "Topman car coat in heritage check", imgsrc: "Images/men_img_3_topman.avif", price:95,category: "Jackets" },
-    {id: 104, name: "River Island brushed stripe cardigan in multi", imgsrc: "Images/men_img_4_river.avif", price:45,category: "Tops"},
-    {id: 105, name: "ASOS DESIGN oversized fit half zip funnel neck check jumper in grey", imgsrc: "Images/men_img_5_oversize.avif", price:38,category: "Tops"},
-    {id: 106, name: "ASOS DESIGN oversized t-shirt with car graphic in cream", imgsrc: "Images/men_img_6_car.avif", price:18, category: "Tops"},
-    {id: 107, name: "Selected Homme Kobe loose fit jeans in off white", imgsrc: "Images/men_img_7_kobe.avif", price:70, category: "Bottoms"},
-    {id: 108, name: "Abercrombie & Fitch short sleeve knit resort button through polo in brown", imgsrc: "Images/men_img_8_abercrombie.avif", price:60, category: "Co-ords"},
+    {id: 101, name: "Aape By A Bathing Ape Now knitted jumper in black", imgsrc: "/Images/207571408-2.avif", price:170, category: "Jackets"},
+    {id: 102, name: "Jack & Jones oversize quarter zip sweatshirt in grey", imgsrc: "/Images/men_img_2_jnj.avif", price:32, category: "Tops"},
+    {id: 103, name: "Topman car coat in heritage check", imgsrc: "/Images/men_img_3_topman.avif", price:95,category: "Jackets" },
+    {id: 104, name: "River Island brushed stripe cardigan in multi", imgsrc: "/Images/men_img_4_river.avif", price:45,category: "Tops"},
+    {id: 105, name: "ASOS DESIGN oversized fit half zip funnel neck check jumper in grey", imgsrc: "/Images/men_img_5_oversize.avif", price:38,category: "Tops"},
+    {id: 106, name: "ASOS DESIGN oversized t-shirt with car graphic in cream", imgsrc: "/Images/men_img_6_car.avif", price:18, category: "Tops"},
+    {id: 107, name: "Selected Homme Kobe loose fit jeans in off white", imgsrc: "/Images/men_img_7_kobe.avif", price:70, category: "Bottoms"},
+    {id: 108, name: "Abercrombie & Fitch short sleeve knit resort button through polo in brown", imgsrc: "/Images/men_img_8_abercrombie.avif", price:60, category: "Co-ords"},
     
 ]
